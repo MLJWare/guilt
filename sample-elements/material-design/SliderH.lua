bbf65413286d789b634371e88601125dfc86a580
MLJWare <mlykkej@gmail.com>
2018-10-11 16:16:07 +0200

    Make "SliderH" template more customizable.

--- sample-elements/material-design/SliderH.lua
+++ sample-elements/material-design/SliderH.lua
@@ -14,14 +14,20 @@ local SliderH = guilt.template("SliderH"):needs{
   progress = pleasure.need.non_negative_number;
 }
 
-local knob_radius      = 6
-local knob_radius_held = 9
+SliderH.knob_radius      = 6
+SliderH.knob_radius_held = 9
 
 SliderH.align_y = 0.5
 
+SliderH.knob_color      = rgb(0, 81, 157)
+SliderH.bar_color_left  = rgb(0, 81, 157)
+SliderH.bar_color_right = rgba(0, 0, 0, 0.5)
+
 function SliderH:init()
   self:set_progress(self.progress)
-  self.preferred_height = knob_radius*2
+  if not self.preferred_height then
+    self.preferred_height = self.knob_radius*2
+  end
 end
 
 function SliderH:set_progress(progress)
@@ -29,18 +35,25 @@ function SliderH:set_progress(progress)
 end
 
 function SliderH:draw()
-  local x1, y, width, height = self:bounds()
-  local x2, knob_x = x1 + width, x1 + width*self.progress
+  local x, y, width, height = self:bounds()
+  local knob_x = x + width*self.progress
+  local knob_y = y + height/2
+  self:draw_bar(x, y, width, height, knob_x, knob_y)
+  self:draw_knob(knob_x, knob_y, x, y, width, height)
+end
 
+function SliderH:draw_knob(knob_x, knob_y, x, y, width, height)
   local radius = self.pressed1
-             and knob_radius_held
-              or knob_radius
+             and self.knob_radius_held
+              or self.knob_radius
 
-  local y2 = y + height/2
-  smooth_line(x1, y2, knob_x, y2, 2, rgb(0, 81, 157))
-  smooth_line(knob_x, y2, x2, y2, 2, rgba(0, 0, 0, 0.5))
-  -- knob
-  smooth_circle(knob_x, y2, radius, rgb(0, 81, 157))
+  smooth_circle(knob_x, knob_y, radius, self.knob_color)
+end
+
+function SliderH:draw_bar(x, y, width, height, knob_x, knob_y)
+  local x2     = x + width
+  smooth_line(x, knob_y, knob_x, knob_y, 2, self.bar_color_left)
+  smooth_line(knob_x, knob_y, x2, knob_y, 2, self.bar_color_right)
 end
 
 function SliderH:mousedragged (mx, my, dx, dy, button1, button2)
@@ -61,6 +74,7 @@ function SliderH:mousepressed (mx, my, button_index)
 
   local x2, knob_x = x1 + width, x1 + width*old_progress
 
+  local knob_radius = self.knob_radius
   if math.abs(knob_x - mx) <= knob_radius then
     -- clicked on the knob, don't update `progress` value
   elseif mx >= x1 - knob_radius and mx <= x2 + knob_radius then
