9f56f2b3b90d9d4f0e9faa4de8502092258850ef
MLJWare <mlykkej@gmail.com>
2018-10-25 14:14:54 +0200

    Change internal representation of PropertyTable.

--- lib/mljware/guilt/sample-elements/standard/PropertyTable.lua
+++ lib/mljware/guilt/sample-elements/standard/PropertyTable.lua
@@ -48,6 +48,8 @@ local function rect_line(x, y, width, height, color)
   love.graphics.rectangle("line", math.floor(x), math.floor(y), math.floor(width), math.floor(height))
 end
 
+local try_invoke = pleasure.try.invoke
+
 local PropertyTable = guilt.template("PropertyTable")
 
 local font = roboto.body2
@@ -68,22 +70,89 @@ local Group = {}
 do
   Group.__index = Group
 
-  function Group:insert_row(key, value, opt_index)
-    if opt_index then
-      table.insert(self._keys  , opt_index, tostring(key   or "") or "")
-      table.insert(self._values, opt_index, tostring(value or "") or "")
+  function Group.new(class, id)
+    local self = setmetatable({
+      id = id;
+      _data = {}
+    }, class)
+    return self
+  end
+
+  function Group:insert_row(key, value, opt_row_index)
+    local data = self._data
+    if opt_row_index then
+      local index = 2*clamp(opt_row_index, 1, #data + 1)
+      table.insert(self._data, index - 1, tostring(key   or "") or "")
+      table.insert(self._data, index    , tostring(value or "") or "")
     else
-      table.insert(self._keys  , tostring(key   or "") or "")
-      table.insert(self._values, tostring(value or "") or "")
+      table.insert(self._data, tostring(key   or "") or "")
+      table.insert(self._data, tostring(value or "") or "")
     end
     return self
+  end
+
+  function Group:get_row(row_index)
+    local data = self._data
+    local index = 2*row_index
+    return data[index - 1], data[index]
+  end
+
+  -- NOTE assumes the row _already_ exists!
+  function Group:set_row(row_index, key, value)
+    local data = self._data
+    local index = 2*row_index
+    data[index - 1] = tostring(key   or "") or ""
+    data[index    ] = tostring(value or "") or ""
+  end
+
+  function Group:get_field(index)
+    return self._data[index]
+  end
+
+  -- NOTE assumes the field _already_ exists!
+  function Group:set_field(index, text)
+    self._data[index] = tostring(text or "") or ""
+  end
+
+  function Group:len()
+    return math.ceil(#self._data/2)
+  end
+
+  function Group:rows()
+    return coroutine.wrap(function ()
+      local data = self._data
+      for row_index = 1, self:len() do
+        local index = row_index*2
+        coroutine.yield(data[index - 1], data[index], row_index)
+      end
+    end)
+  end
+end
+
+local Groups = {}
+do
+  Groups.__index = Groups
+
+  function Groups.new(class)
+    local self = setmetatable({}, class)
+    return self
+  end
+
+  function Groups:add_group(group_id, opt_group_index)
+    local group = Group:new(group_id)
+    if opt_group_index then
+      table.insert(self, opt_group_index, group)
+    else
+      table.insert(self, group)
+    end
+    return group
   end
 end
 
 function PropertyTable:init()
   self.keys_id   = self.keys_id   or "Property"
   self.values_id = self.values_id or "Value"
-  self._groups   = {}
+  self._groups   = Groups:new()
   self._field = {
     text = "";
     hint = "";
@@ -97,14 +166,8 @@ function PropertyTable:init()
   self.split_pct = 0.4
 end
 
-function PropertyTable:add_group(group_id, opt_index)
-  local group = setmetatable({id = group_id, _keys = {}, _values = {}}, Group)
-  if opt_index then
-    table.insert(self._groups, opt_index, group)
-  else
-    table.insert(self._groups, group)
-  end
-  return group
+function PropertyTable:add_group(group_id, opt_group_index)
+    return self._groups:add_group(group_id, opt_group_index)
 end
 
 function PropertyTable:group(group_id)
@@ -119,14 +182,13 @@ function PropertyTable:_find_field_at(mx, my)
   for _, group in ipairs(self._groups) do
     local new_dy = dy + row_height
     if not group.collapsed then
-      new_dy = new_dy + #group._keys*row_height
+      new_dy = new_dy + group:len()*row_height
     end
     if new_dy > my then
       local split_width = self.split_pct*width
-      local column      = (mx < split_width) and "_keys" or "_values"
-      local index       = math.floor((my - dy)/row_height)
-
-      return group, column, index
+      local row_index    = math.floor((my - dy)/row_height)
+      local index = (mx < split_width) and row_index*2 - 1 or row_index*2
+      return group, index
     end
     dy = new_dy
   end
@@ -134,98 +196,88 @@ end
 
 function PropertyTable:_active_field_bounds()
   local group  = self._active_group
-  local column = self._active_column
-  local index  = self._active_index
-  if not (group and column and index) then return 0,0,0,0 end
+  local index = self._active_index
+  if not (group and index) then return 0,0,0,0 end
 
   local _, _, width = self:bounds()
   local split_width = self.split_pct*width
   local dx, field_width
-  if column == "_values" then
-    dx, field_width = split_width, width - split_width
-  else
+  if index%2 == 1 then
     dx, field_width = 0, split_width
+  else
+    dx, field_width = split_width, width - split_width
   end
 
-  local dy = row_height*index
+  local dy = row_height*math.floor((index+1)/2)
   for _, group2 in ipairs(self._groups) do
     dy = dy + row_height
     if group == group2 then break end
     if not group2.collapsed then
-      dy = dy + #group2._keys*row_height
+      dy = dy + group2:len()*row_height
     end
   end
 
   return dx, dy, field_width, row_height
 end
 
-function PropertyTable:_set_active_field(group, column, index)
+function PropertyTable:_set_active_field(group, index)
   local active_group  = self._active_group
-  local active_column = self._active_column
   local active_index  = self._active_index
 
-  if (active_group and active_column and active_index) then
-    local it = active_group[active_column]
-    if it then
-      it[active_index] = self._field.text
+  if active_group and active_index then
+    local old_text = active_group:get_field(active_index)
+    local new_text = self._field.text
+    active_group:set_field(active_index, new_text)
+    if old_text ~= new_text then
+      try_invoke(self, "on_field_change", active_group, active_index, old_text, new_text)
     end
   end
 
   self._edit_:set_text("")
   self._active_group  = group
-  self._active_column = column
   self._active_index  = index
-  if not group then return end
-  local data = group[column]
-  self._field.text = tostring(data and data[index] or "")
+  if not (group and index) then return end
+  self._field.text = group:get_field(index)
 end
 
 function PropertyTable:mousepressed(mx, my, button, isTouch)
-  local group, column, index = self:_find_field_at(mx, my)
+  local group, index = self:_find_field_at(mx, my)
   self.active = true
 
-  if  self._active_group  == group
-  and self._active_column == column
-  and self._active_index  == index then
+  if  group == self._active_group
+  and index == self._active_index then
     local dx, dy = self:bounds()
     self._edit_:mousepressed(mx - dx, my - dy, button, isTouch)
   else
     if group and index and index <= 0 then
       group.collapsed = not group.collapsed
-      self:_set_active_field(nil, nil, nil)
+      self:_set_active_field(nil, nil)
       self.active = false
       return
     end
-    self:_set_active_field(group, column, index)
+    self:_set_active_field(group, index)
     local dx, dy = self:bounds()
     self._edit_:mousepressed(mx - dx, my - dy, button, isTouch)
   end
 end
 
 function PropertyTable:has_active_field()
-  return (self._active_group and self._active_column and self._active_index)
-    and true or false
+  return (self._active_group and self._active_index) and true or false
 end
 
 function PropertyTable:textinput(input)
-  if not self:has_active_field() then
-    return
-  end
+  if not self:has_active_field() then return end
   self._edit_:textinput(input)
 end
 
 function PropertyTable:keypressed(key, scancode, isrepeat)
-  if not self:has_active_field() then
-    return
-  end
+  if not self:has_active_field() then return end
   self._edit_:keypressed(key, scancode, isrepeat)
-  pleasure.try.invoke(self, "on_keypressed", key, scancode, isrepeat)
+  try_invoke(self, "on_keypressed", key, scancode, isrepeat)
 end
 
 function PropertyTable:mousedragged(mx, my, dx, dy, button1, button2)
-  if not self:has_active_field() then
-    return
-  end
+  if not self:has_active_field() then return end
   local x, y = self:bounds()
   self._edit_:mousedragged(mx - x, my - y, dx, dy, button1, button2)
 end
@@ -283,16 +335,13 @@ function PropertyTable:draw_group(group, width, height, split_width, dy)
   local group_height = row_height
 
   local group_is_active = self._active_group == group
-  local active_index  = self._active_index
-  local active_column_is_key = self._active_column == "_keys"
+  local active_index    = self._active_index
 
-  for index, key in ipairs(group._keys) do
-    local row_is_active = group_is_active and (active_index == index)
-    local key_is_active   = row_is_active and active_column_is_key
-    local value_is_active = row_is_active and not active_column_is_key
+  for key, value, row_index in group:rows() do
+    local key_is_active   = group_is_active and (row_index*2 - 1 == active_index)
+    local value_is_active = group_is_active and (row_index*2     == active_index)
 
     self:draw_field(key, 0, group_height + dy, split_width, key_is_active)
-    value = group._values[index]
     self:draw_field(value, split_width, group_height + dy, width - split_width, value_is_active)
     hline(0, dy + group_height, width, outline_color)
 
@@ -313,7 +362,7 @@ function PropertyTable:draw_field(field_text, x, y, width, is_active)
     end
     return
   end
-  love.graphics.setColor(is_active and rgb(201, 197, 90) or rgb(0,0,0))
+  love.graphics.setColor(rgb(0,0,0))
   pleasure.push_region(x, y, width, row_height)
   font_writer.print_aligned(font, field_text, x_pad, y_pad, "left", "top")
   pleasure.pop_region()
