c1ebbefe0418e7e0856af6d067d6c88f68a909ae
MLJWare <mlykkej@gmail.com>
2018-11-18 11:06:33 +0100

    Pressed tag only set when appropriate

--- lib/mljware/guilt/delegate/mousepressed.lua
+++ lib/mljware/guilt/delegate/mousepressed.lua
@@ -22,21 +22,19 @@ return function (self, mx, my, button, isTouch)
   local gui = self._guilt_gui_
   local gui_tag_bag = ensure(gui.tags, press_tag)
 
-  local not_found = true
   for _, child, region_x, region_y, region_width, region_height in self:children() do
     local child_mx, child_my = mx - (region_x or 0), my - (region_y or 0)
     -- TODO ensure [mx, my] contained in region
     child.active = nil
-    if  child_mx >= 0 and (region_width or math.huge) > child_mx
-    and child_my >= 0 and (region_height or math.huge) > child_my
+    if  child_mx >= 0 and (region_width or math.huge) >= child_mx
+    and child_my >= 0 and (region_height or math.huge) >= child_my
     and child:contains(child_mx, child_my) then
       gui_tag_bag[child] = true
       child[press_tag] = true
       child.pressed    = true
-      if  not_found
-      and is_callable(child.mousepressed) then
+      if is_callable(child.mousepressed) then
         child:mousepressed(child_mx, child_my, button, isTouch)
-        not_found = false
+        break
       end
     end
   end
